37d7975156bd9c3d27c186a1f6885db0d6859edf
Saúl Valdelvira <saul@saulv.es>
2025-05-28 09:43:32 +0200

    Run miri on lint script

--- lint.sh
+++ lint.sh
@@ -2,9 +2,10 @@
 
 echo "Linting..."
 
-((cargo --color=always test --all --all-features && \
+((cargo +nightly --color=always test --all --all-features && \
 cargo --color=always clippy -- -D warnings && \
-cargo --color=always doc --workspace --all-features --no-deps --document-private-items) &>/tmp/lint.txt ) || (
+cargo +nightly --color=always miri test --all --all-features && \
+cargo +nightly --color=always doc --workspace --all-features --no-deps --document-private-items) &>/tmp/lint.txt ) || (
     cat /tmp/lint.txt
     echo "YOU CAN'T COMMIT THIS GARBAGE!"
     exit 1
